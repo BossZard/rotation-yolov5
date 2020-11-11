f13c369ef225f9cf8a890b986d2299bbfe013d48
liangjunjie <7662684+liangjunjie97@user.noreply.gitee.com>
2020-11-11 16:48:36 +0800

    update detect.py cfg

--- detect.py
+++ detect.py
@@ -150,9 +150,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='weights/m-70.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/edge_pic', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='inference/edge_out-m-100', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=1224, help='inference size (pixels)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=1024, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
